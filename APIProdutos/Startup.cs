5820238c4ed157e6e5d3bc3a08a4e3a94a484c96
Renato Groffe <renato.groffe@yahoo.com.br>
2020-02-08 16:32:14 -0300

    ASP.NET Core 3.1 + Remoção Nulls + Compressão GZip
    ASP.NET Core 3.1 + Remoção Nulls + Compressão GZip

---
+++ APIProdutos/Startup.cs
@@ -0,0 +1,60 @@
+using System.IO.Compression;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace APIProdutos
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Configura o modo de compressão
+            services.Configure<GzipCompressionProviderOptions>(
+                options => options.Level = CompressionLevel.Optimal);
+            services.AddResponseCompression(options =>
+            {
+                options.Providers.Add<GzipCompressionProvider>();
+                options.EnableForHttps = true;
+            });
+
+            // Remoção de valores nulos
+            services.AddControllers().AddJsonOptions(opcoes =>
+            {
+                opcoes.JsonSerializerOptions.IgnoreNullValues = true;
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+            
+            // Ativa a compressão
+            app.UseResponseCompression();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
